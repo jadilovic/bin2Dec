10369de2f8b8fddb4cb5e1644ff92559193fa7f3
jadilovic <adilovic79@yahoo.com>
2019-08-06 22:45:08 +0200

    addOwner 2

--- addOwner.java
+++ addOwner.java
@@ -1,7 +1,10 @@
 package vjezbe;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class addOwner {
 
@@ -38,7 +41,55 @@ public class addOwner {
 		for(int i = 0; i < fileChaps.size(); i++){
 			System.out.println(fileChaps.get(i).getName());
 		}
+		
+		if(fileChaps.isEmpty()){
+			System.out.println("There is no directories in direoctoy " + chaps.getName() + " that have chapter in it");
+			System.exit(3);
+		}
+		
+		for(File dir: fileChaps){
+			File[] files = dir.listFiles();
 			
+			if(files == null) continue;
+			
+			for(File file: files){
+				if(file.getName().contains(".java"))
+					owner(file, "jasmin");
+			}
+		}
+			
+	}
+
+	private static void owner(File file, String string) {
+		String lineSeparator = "\n";
+		String addOwnerName = "Code made by: " + string + ";";
+		
+		try {
+			Scanner input = new Scanner(file);
+			while(input.hasNextLine()){
+				String s = input.nextLine();
+				if(s.equals(addOwnerName)){
+					System.out.println("The code in " + file.getName() + " already has owner name");
+					return;
+				}
+				addOwnerName += s + lineSeparator;
+				System.out.print(addOwnerName);
+			}
+			input.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try{
+			PrintWriter output = new PrintWriter(file);
+			output.write(addOwnerName);
+			output.close();
+		} catch (FileNotFoundException e){
+			e.printStackTrace();
+		}
+		
 	}
 
 }
